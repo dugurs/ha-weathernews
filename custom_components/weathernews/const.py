3bd1846f05d8163be6d14347beaacbc57ad9a1c6
dugurs <dugurs@gmail.com>
2024-11-21 22:32:01 +0900

    iconCode 추가

--- custom_components/weathernews/const.py
+++ custom_components/weathernews/const.py
@@ -46,23 +46,36 @@ LANG_CODES = ['ko-KR', 'en-US']
 # 202 흐리고 한때 비
 # 204 흐리고 한때 눈
 # 300 비
+# 301 맑지만 한때 비
 # 302 한때 비
 # 303 진눈깨비
+# 308 빗발
 # 400 눈
+# 401 눈 해
+# 402 눈 구름
+# 403 눈 비
+# 500 맑은 밤
+# 501 대체로 맑은 밤
+# 502 맑은 밤 한때 비
+# 502 대체로 맑은 밤 한때 비
+# 504 대체로 맑은 밤 한때 눈
+
+# https://www.kr-weathernews.com/mv4/html/assets/images/weather-icon-set/icon1/light/day/400.svg
+# https://www.jma.go.jp/bosai/forecast/img/400.svg
 
 ICON_CONDITION_MAP: Final[dict[str, list[int]]] = {
-    ATTR_CONDITION_CLEAR_NIGHT: [1100],  # 맑은 밤
+    ATTR_CONDITION_CLEAR_NIGHT: [1100, 500, 501],  # 맑은 밤
     ATTR_CONDITION_CLOUDY: [200],       # 많은 구름 
     ATTR_CONDITION_EXCEPTIONAL: [],     # 특별한 44 is Not Available (N/A)
     ATTR_CONDITION_FOG: [],             # 안개
-    ATTR_CONDITION_HAIL: [],            # 빗발
+    ATTR_CONDITION_HAIL: [308],            # 빗발
     ATTR_CONDITION_LIGHTNING: [],       # 번개/뇌우
     ATTR_CONDITION_LIGHTNING_RAINY: [], # 번개/천둥, 비
     ATTR_CONDITION_PARTLYCLOUDY: [101, 102, 104, 201, 202],    # 구름 몇 개 
     ATTR_CONDITION_POURING: [],         # 쏟아지는 비
-    ATTR_CONDITION_RAINY: [300, 302],        # 비
-    ATTR_CONDITION_SNOWY: [204, 400],           # 눈
-    ATTR_CONDITION_SNOWY_RAINY: [303],     # 눈과 비
+    ATTR_CONDITION_RAINY: [300, 301, 302, 502],        # 비
+    ATTR_CONDITION_SNOWY: [204, 400, 401, 402, 504],           # 눈
+    ATTR_CONDITION_SNOWY_RAINY: [303, 403],     # 눈과 비
     ATTR_CONDITION_SUNNY: [100],        # 햇빛
     ATTR_CONDITION_WINDY: [],           # 바람
     ATTR_CONDITION_WINDY_VARIANT: []    # 바람과 구름
